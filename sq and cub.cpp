5203b0fe05f7c0cf12b44ed42f07b284cea38acc
SUNIL SHURAJ N <118829941+shuraj03@users.noreply.github.com>
2023-01-24 14:14:57 +0530

    Add files via upload

---
+++ sq and cub.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	float a,sq,cb;
+	printf("Enter the value of a :");
+	scanf("%f",&a);
+	sq=a*a;
+	printf("Square Number%f=%f",a,sq);
+	cb=a*a*a;
+	printf("\ncube number %f=%f",a,cb);
+}
